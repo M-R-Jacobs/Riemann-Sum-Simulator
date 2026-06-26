45b67b006042f812b468b3776b629d901a61146f
M-R-Jacobs <mjacobs0002@gmail.com>
2026-06-26 12:56:19 -0700

    Initial definitions & intervals

---
+++ Reimann.py
@@ -0,0 +1,25 @@
+# Library imports
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.widgets import TextBox, RadioButtons
+
+
+
+# Define the function
+
+def f(x):
+    return -(x - 3) ** 2 + 5
+
+A = 1.0
+B = 5.0 
+    # Hard-coded integration bounds [A,B]. Keeping these as named constants for later
+
+def compute_approximation(method, n): #n subintervals, with the given method: 'left', 'right', 'mid', or 'trap'.
+    dx = (B - A) / n
+    x_edges = np.linspace(A, B, n + 1)
+
+# try elif method == ...
+
+    return approx_value, x_edges 
+        # the numeric approximation and x-coords of the subinterval boundaries
+        # (used later for drawing the rectangles/trapezoids)
